60950c975b12b26fd5bb04e0b4ed5a2ee685ed3b
maximino_guerra <maximino_guerra@hotmail.com>
2018-04-22 21:54:19 -0600

    Metadata

--- Entities/BlogPost.cs
+++ Entities/BlogPost.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public class BlogPost
+    public partial class BlogPost
     {
 
         public int Id { get; set; }
---
+++ Entities/BlogPostMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [MetadataType(typeof(Metadata))]
+    partial class BlogPost
+    {
+        private sealed class Metadata
+        {
+            [Required]
+            public string Titulo { get; set; }
+        } 
+    }
+}
